cc170c902da24312b57cbb447835ee36e1e5fe12
yuki hara <hara.0509@icloud.com>
2021-01-23 17:57:32 +0900

    5-2終了

--- sns/admin.py
+++ sns/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Message, Group, Friend, Good
 
 # Register your models here.
+admin.site.register(Message)
+admin.site.register(Friend)
+admin.site.register(Group)
+admin.site.register(Good)
