301c572876f030dd1935102cc5cdc0e723461cac
yuki hara <hara.0509@icloud.com>
2021-01-23 18:25:34 +0900

    hello出力成功

--- django_app/settings.py
+++ django_app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hello',
     'sns',
+    'crud_pra',
 ]
 
 MIDDLEWARE = [
--- django_app/urls.py
+++ django_app/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include('hello.urls')),
+    path('crud_pra', include('crud_pra.urls')),
 ]
