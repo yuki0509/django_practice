a68f62d65cdd8d008aeef676f5e42cc5e6290240
yuki hara <hara.0509@icloud.com>
2021-01-25 05:04:11 +0900

    django_heroku位置修正

--- django_app/settings.py
+++ django_app/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -33,6 +32,7 @@ except ImportError:
 
 if not DEBUG:
   SECRET_KEY = os.environ['SECRET_KEY']
+  import django_heroku
   django_heroku.settings(locals())
 
 
